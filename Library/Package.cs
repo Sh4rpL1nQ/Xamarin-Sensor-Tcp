7efc50888aafdab72f3f8f4290590589d6610d12
Dominik Weder <dodo.weder@gmail.com>
2020-03-21 13:52:03 +0100

    Definded the package type to be sent

---
+++ Library/Package.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    [Serializable]
+    public class Package
+    {
+        public List<object> data;
+        public string senderId;
+        public PackageType packetType;
+
+        public Package(PackageType packetType, string senderId)
+        {
+            this.packetType = packetType;
+            this.senderId = senderId;
+
+            data = new List<object>();
+        }
+
+        public Package(byte[] bytes)
+        {
+            var formatter = new BinaryFormatter();
+            var mem = new MemoryStream(bytes);
+
+            Package p = (Package)formatter.Deserialize(mem);
+
+            data = p.data;
+            packetType = p.packetType;
+            senderId = p.senderId;
+
+            mem.Close();
+        }
+
+        public byte[] ToBytes()
+        {
+            var formatter = new BinaryFormatter();
+            var mem = new MemoryStream();
+
+            formatter.Serialize(mem, this);
+            var bytes = mem.ToArray();
+            mem.Close();
+
+            return bytes;
+        }
+
+        public static string GetIpAdress()
+        {
+            IPAddress[] ips = Dns.GetHostAddresses(Dns.GetHostName());
+
+            foreach (var ip in ips)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                    return ip.ToString();
+            }
+
+            return "127.0.0.1";
+        }
+    }
+}
